b9d03da939b81d00e508ea4416b4516c298fb9e7
jhwang0614 <jhwang0614@gmail.com>
2016-04-14 23:46:49 -0400

    admin_area pictures

---
+++ html_template/admin_area/includes/db.php
@@ -0,0 +1,6 @@
+<?php
+$con = mysqli_connect("db.soic.indiana.edu","caps16_team11","my+sql=caps16_team11","caps16_team11");
+
+
+
+?>
---
+++ html_template/admin_area/insert_product.php
@@ -0,0 +1,79 @@
+<!DOCTYPE>
+
+<?php
+include("includes/db.php");
+?>
+<html>
+	<head>
+		<title>Inserting Product</title>
+		<script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
+  <script>tinymce.init({ selector:'textarea' });</script>
+	</head>
+<body bgcolor="skyblue">
+	<form action="insert_product.php" method="post" enctype="multipart/form-data">
+		
+		<table align="center" width="700" border="2" bgcolor="pink">
+			<tr align="center">
+				<td colspan="7"><h2>Insert New Post Here</h2></td>
+			</tr>
+			
+			<tr>
+				<td align="right"><b>Product Title:</b></td>
+				<td><input type="text" name="product_title" size="60" required/></td>
+			</tr>
+			<tr>
+				<td align="right"><b>Product Brand:</b></td>
+				<td>
+				<select name="product_brand">
+					<option>Select a Brand</option>
+					<?php
+					$get_brands = "select * from brands";
+	$run_brands = mysqli_query($con, $get_brands);
+	while ($row_brands=mysqli_fetch_array($run_brands)){
+		$brand_id = $row_brands['brand_id'];
+		$brand_title = $row_brands['brand_title'];
+		
+	echo "<option value='$brand_id'>$brand_title</option>";
+}
+					?>
+				</td>
+			</tr><tr>
+				<td align="right"><b>Product Image:</b></td>
+				<td><input type="file" name="product_image" /></td>
+			</tr><tr>
+				<td align="right"><b>Product Price:</b></td>
+				<td><input type="text" name="product_price" required/></td>
+			</tr><tr>
+				<td align="right"><b>Product Description:</b></td>
+				<td><textarea name="product_desc" cols="20" rows="10"></textarea></td>
+			</tr>
+			<tr align="center">
+				<td colspan="8"><input type="submit" name="insert_post" value="Insert Product Now"/></td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
+<?php
+	if(isset($_POST['insert_post'])){
+		//getting the text data from the fields
+		$product_title = $_POST['product_title'];
+		$product_brand = $_POST['product_brand'];
+		$product_price = $_POST['product_price'];
+		$product_desc = $_POST['product_desc'];
+		
+		//getting the image from the field
+		$product_image = $_FILES['product_image']['name'];
+		$product_image_tmp = $_FILES['product_image']['tmp_name'];
+		
+		move_uploaded_file($product_image_tmp,"product_images/$product_image");
+		
+		$insert_product = "insert into products (product_brand,product_title,product_price,product_desc,product_image) values ('$product_brand','$product_title','$product_price','$product_desc',$product_image')";
+		
+		$insert_pro = mysqli_query($con, $insert_product);
+		if($insert_pro){
+			echo "<script>alert('Product Has been Inserted!')</script>";
+			echo "<script>window.open('insert_product.php','_self')</script>";
+		}
+	}
+?>
